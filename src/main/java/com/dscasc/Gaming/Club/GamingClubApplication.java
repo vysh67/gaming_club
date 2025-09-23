9e78fca25fbfd616f339607875c3ba7437fe88e0
vysh67 <gvyshu03@gmail.com>
2025-09-23 13:59:57 +0530

    initial commit

---
+++ src/main/java/com/dscasc/Gaming/Club/GamingClubApplication.java
@@ -0,0 +1,13 @@
+package com.dscasc.Gaming.Club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dscasc/Gaming/Club/GamingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dscasc.Gaming.Club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
